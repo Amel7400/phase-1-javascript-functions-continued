1428ade419d77d7a10cafdf361e91b04a0d54286
AMEL BATOUCHE <amelbat7400@gmail.com>
2023-08-23 20:50:19 +0200

    Assignement Completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(fun = 'roller-skate'){
+return `This Saturday, I want to ${fun}!`;
+}
+saturdayFun();
+saturdayFun(fun);
+
+function mondayWork(work = "go to the office"){
+  return `This Monday, I will ${work}.`
+}
+console.log(mondayWork());
+mondayWork("work from home");
+
+function wrapAdjective(highlight = "*"){
+  return function(thus = "special"){
+    return  `You are ${highlight}${thus}${highlight}!`
+  }
+}
+wrapAdjective("*")("a hard worker")
+wrapAdjective("||")("a dedicated programmer")
